1e54c29c19815fb521fca6e98cdd6098155fa2c3
techswarn <tech.swarns@gmail.com>
2022-12-15 14:15:18 +0530

    code update

--- config/database.js
+++ config/database.js
@@ -1,12 +1,26 @@
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'mysql',
+//     connection: {
+//       host: env('DATABASE_HOST', '127.0.0.1'),
+//       port: env.int('DATABASE_PORT', 3306),
+//       database: env('DATABASE_NAME', 'store'),
+//       user: env('DATABASE_USERNAME', 'root'),
+//       password: env('DATABASE_PASSWORD', '$uCC12@my$qle$$'),
+//       ssl: env.bool('DATABASE_SSL', false),
+//     },
+//   },
+// });
+
 module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 3306),
+      host: env('DATABASE_HOST', 'db-mysql-blr-do-user-11648032-0.b.db.ondigitalocean.com'),
+      port: env.int('DATABASE_PORT', 25060),
       database: env('DATABASE_NAME', 'store'),
-      user: env('DATABASE_USERNAME', 'root'),
-      password: env('DATABASE_PASSWORD', '$uCC12@my$qle$$'),
+      user: env('DATABASE_USERNAME', 'swarn'),
+      password: env('DATABASE_PASSWORD', 'AVNS_Ca8cvIzONVZgfSdNUwZ'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
