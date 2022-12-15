d6e6f795deb0321ae605b6c8f670105a478166d5
techswarn <tech.swarns@gmail.com>
2022-12-15 20:17:01 +0530

    code update

--- config/database.js
+++ config/database.js
@@ -1,27 +1,13 @@
-// module.exports = ({ env }) => ({
-//   connection: {
-//     client: 'mysql',
-//     connection: {
-//       host: env('DATABASE_HOST', '127.0.0.1'),
-//       port: env.int('DATABASE_PORT', 3306),
-//       database: env('DATABASE_NAME', 'store'),
-//       user: env('DATABASE_USERNAME', 'root'),
-//       password: env('DATABASE_PASSWORD', '$uCC12@my$qle$$'),
-//       ssl: env.bool('DATABASE_SSL', false),
-//     },
-//   },
-// });
-
 module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST', 'db-mysql-blr-do-user-11648032-0.b.db.ondigitalocean.com'),
-      port: env.int('DATABASE_PORT', 25060),
-      database: env('DATABASE_NAME', 'store'),
-      user: env('DATABASE_USERNAME', 'swarn'),
-      password: env('DATABASE_PASSWORD', 'AVNS_Ca8cvIzONVZgfSdNUwZ'),
-      ssl: env.bool('DATABASE_SSL', false),
+      host: env('DATABASE_HOST', process.env.DATABASE_HOST),
+      port: env.int('DATABASE_PORT', process.env.DATABASE_PORT),
+      database: env('DATABASE_NAME', process.env.DATABASE_NAME),
+      user: env('DATABASE_USERNAME', process.env.DATABASE_USERNAME),
+      password: env('DATABASE_PASSWORD', process.env.DATABASE_PASSWORD),
+      ssl: env.bool('DATABASE_SSL', process.env.DATABASE_SSL),
     },
   },
 });
